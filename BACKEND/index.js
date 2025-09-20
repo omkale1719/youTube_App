da4652165d1e7e0cf728eab241c20ff09a5bea10
omkale1719 <omk28035@gmail.com>
2025-09-21 00:03:59 +0530

    file  add

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,32 +1,36 @@
-
 var express = require("express");
 var app = express();
-var port = 4000
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
+var port = 4000;
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
-app.use(cors({
-    origin: 'http://localhost:5173'||'https://youtube-app-1-unsu.onrender.com',
-    credentials: true
-  }))
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://youtube-app-1-unsu.onrender.com",
+    ],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(cookieParser());
-require('./Connection/conn');
+require("./Connection/conn");
 
-const AuthRoutes = require('./Routes/user');
-const VideoRoutes = require('./Routes/video');
-const CommentRoutes = require('./Routes/comment');
+const AuthRoutes = require("./Routes/user");
+const VideoRoutes = require("./Routes/video");
+const CommentRoutes = require("./Routes/comment");
 
-app.use('/user', AuthRoutes);  
-app.use('/auth',AuthRoutes);
-app.use('/api',VideoRoutes);
-app.use('/commentApi',CommentRoutes);
-
+app.use("/user", AuthRoutes);
+app.use("/auth", AuthRoutes);
+app.use("/api", VideoRoutes);
+app.use("/commentApi", CommentRoutes);
 
 app.get("/", (req, res) => {
   res.redirect("/api/allVideo");
 });
 
-
-app.listen(port,()=>{console.log("Our backend project is running on Port 4000")});
+app.listen(port, () => {
+  console.log("Our backend project is running on Port 4000");
+});
